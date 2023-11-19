c1fdc1043912dbae69698066b253fe06713c8b14
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-11-20 00:14:46 +0300

    Finish : Module #3. Task #4. Primitives

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        double a = 6.8;
+
+        double b = 8.9;
+
+        double result;
+
+        result = ( 9*a*a - 5*b + 2 + a - 7 )*( (a +b - 4*a*b) / 2 );
+
     }
 }
